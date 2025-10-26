697bda08b2f2282a22c5b39545441f182002dc6f
Ubuntu <ubuntu@ip-172-31-16-190.ec2.internal>
2025-10-26 02:28:14 +0000

    small change for webhook

--- server.js
+++ server.js
@@ -4,3 +4,4 @@ const port = process.env.PORT || 8080;
 app.get('/', (req, res) => res.send(`Hello from FSL Challenge | Secret: ${process.env.MY_SECRET || 'not-set'}`));
 app.listen(port, () => console.log(`Running on ${port}`));
 
+hello
